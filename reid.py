b697ca18c94b55c3bb5ffe656bd94549b21d72f9
dpj <983234933@qq.com>
2025-03-13 09:30:03 +0800

    code commit

--- reid.py
+++ reid.py
@@ -25,9 +25,6 @@ def logger_info(info):
     # print(info)
     logger.info(info)
 
-
-
-
 batch_size = 32
 torch.manual_seed(1234)
 rng = torch.Generator()
@@ -35,74 +32,60 @@ rng.manual_seed(1234)
 text_dataset = Dydata(r"../data_processed3/test")
 #text_dataset = Dydata(r"../s")
 
-
 query_ratio = 0.3
 query_size = int(len(text_dataset)*query_ratio)
 gallery_size = len(text_dataset)-query_size
 
 query_dataset, gallery_dataset = random_split(text_dataset, [query_size, gallery_size], generator=rng)
 
-
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
 querylaoder = DataLoader(query_dataset, batch_size)
 gallerylaoder = DataLoader(gallery_dataset, batch_size)
-
 pointg = torch.load(r"state/model_34.pth")
-
 pointg = pointg.to(device)
 
+pointg.eval()
+with torch.no_grad():
+    qf, q_pids= [], []
+    for data in querylaoder:
+        input, label = data
+        input = input.to(torch.float)
+        input = input.to(device)
+
+        feature= pointg(input, None)
+
+        qf.append(feature)
+        q_pids.extend(label)
 
 
-for epoch in range(500):
-    print("------------------第{}轮测试---------------".format(epoch + 1))
+    qf = torch.cat(qf, 0).cpu()
+    qf = np.asarray(qf)
+    q_pids = np.asarray(q_pids)
 
-    pointg.eval()
-    with torch.no_grad():
-        qf, q_pids= [], []
-        for data in querylaoder:
-            input, label = data
-            input = input.to(torch.float)
-            input = input.to(device)
+    gf, g_pids = [], []
 
+    for data in gallerylaoder:
+        input, label = data
+        input = input.to(torch.float)
+        input = input.to(device)
 
-            feature= pointg(input, None)
+        feature = pointg(input, None)
 
-            qf.append(feature)
-            q_pids.extend(label)
-            
-            
-    
+        gf.append(feature)
+        g_pids.extend(label)
 
-        qf = torch.cat(qf, 0).cpu()
-        qf = np.asarray(qf)
-        q_pids = np.asarray(q_pids)
+    gf = torch.cat(gf, 0).cpu()
+    gf = np.asarray(gf)
+    g_pids = np.asarray(g_pids)
 
-        gf, g_pids = [], []
+    if np.isnan(qf).any():
+        qf[np.isnan(qf)] = 0
 
-        for data in gallerylaoder:
-            input, label = data
-            input = input.to(torch.float)
-            input = input.to(device)
+    if np.isnan(gf).any():
+        gf[np.isnan(gf)] = 0
 
-            feature = pointg(input, None)
-
-            gf.append(feature)
-            g_pids.extend(label)
-
-        gf = torch.cat(gf, 0).cpu()
-        gf = np.asarray(gf)
-        g_pids = np.asarray(g_pids)
-
-        if np.isnan(qf).any():
-            qf[np.isnan(qf)] = 0
-
-        if np.isnan(gf).any():
-            gf[np.isnan(gf)] = 0
-
-
-        top1_acc, top3_acc, topk_acc, mAP = evaluate(qf, gf, q_pids, g_pids)
-        print("第{}次测试结果:Top-1 Accuracy:{},Top-3 Accuracy:{} Top-5 Accuracy:{},mAP:{}".format(epoch+1,top1_acc,top3_acc,topk_acc,mAP))
+    top1_acc, top3_acc, topk_acc, mAP = evaluate(qf, gf, q_pids, g_pids)
+    print("测试结果:Top-1 Accuracy:{},Top-3 Accuracy:{} Top-5 Accuracy:{},mAP:{}".format(top1_acc,top3_acc,topk_acc,mAP))
 
 
 
